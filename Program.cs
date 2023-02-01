151a57f87c1e99da32151c4012bfb1a1c137e5c3
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-02-01 09:13:57 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace System.IO_streamPisac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upišite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            sw.WriteLine("Ime: " + ime);
+            sw.WriteLine("Prezime: " + prezime);
+            sw.Close();
+
+            StreamReader sr = new StreamReader(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            while(!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
